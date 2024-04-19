575f1b07907113a74797fe3977e7842f745091b2
Josh Crawford <joshua@sgroup.com.au>
2024-04-19 22:16:50 +1000

    Update English translations.

--- src/translations/en/bugsnag.php
+++ src/translations/en/bugsnag.php
@@ -1,14 +1,5 @@
 <?php
 
 return [
-    'Bugsnag' => 'Bugsnag',
-    'Enabled' => 'Enabled',
-    'Enable exception logging.' => 'Enable exception logging.',
-    'Server API key' => 'Server API key',
-    'You can find the API key immediately after creating a new project from your Bugsnag dashboard, or later on your project’s settings page.' => 'You can find the API key immediately after creating a new project from your Bugsnag dashboard, or later on your project’s settings page.',
-    'Browser API key' => 'Browser API key',
-    'Release stage' => 'Release stage',
-    'Blacklist' => 'Blacklist',
-    'Disable certain types of exceptions. Class have to be fully namespaced. (i.e. \\yii\\web\\NotFoundHttpException)' => 'Disable certain types of exceptions. Class have to be fully namespaced. (i.e. \\yii\\web\\NotFoundHttpException)',
-    'Enable exception logging' => 'Enable exception logging',
+  'Bugsnag' => 'Bugsnag',
 ];
