b89d38c7e5c7b6a37827af4442da68783706ad0a
Josh Crawford <joshua@sgroup.com.au>
2024-05-23 13:46:46 +1000

    Update English translations.

--- src/translations/en/bugsnag.php
+++ src/translations/en/bugsnag.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
+  'Blacklist' => 'Blacklist',
+  'Browser API key' => 'Browser API key',
   'Bugsnag' => 'Bugsnag',
+  'Enabled' => 'Enabled',
+  'Enable exception logging.' => 'Enable exception logging.',
+  'Release stage' => 'Release stage',
+  'Server API key' => 'Server API key',
+  'You can find the API key immediately after creating a new project from your Bugsnag dashboard, or later on your project’s settings page.' => 'You can find the API key immediately after creating a new project from your Bugsnag dashboard, or later on your project’s settings page.',
 ];
